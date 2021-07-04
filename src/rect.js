7394b622cc7575b90db23443859ae731e221e968
ReusLi <574399934@qq.com>
2021-07-04 19:54:34 +0800

    init window mouse event

--- src/rect.js
+++ src/rect.js
@@ -3,3 +3,7 @@
 // 根据两个坐标构造出矩形框
 
 // 构建svg
+
+window.addEventListener('mousedown', (event) => {});
+window.addEventListener('mousemove', (event) => {});
+window.addEventListener('mouseup', (event) => {});
