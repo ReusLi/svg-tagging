47d0e4dcff84edf71d7b4cabd18a2acc5da6c967
ReusLi <574399934@qq.com>
2021-08-02 21:40:25 +0800

    add css

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,85 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  body *,
+  html * {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  .c-draw-rect,
+  .c-draw-size-label,
+  .c-draw-color-label,
+  .c-draw-line,
+  svg {
+    position: absolute;
+  }
+
+  .svg-panel {
+    overflow: visible;
+  }
+
+  .region-text {
+    font-size: 12px;
+  }
+
+  .c-draw-size-label>svg {
+    position: absolute;
+    margin: auto;
+    overflow: visible;
+    min-height: 2px;
+    min-width: 2px;
+    pointer-events: none;
+  }
+
+  .c-draw-size-label {
+    font-size: 12px;
+  }
+
+  .c-draw-color-label .color-text {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    margin-left: 5px;
+  }
+
+  .c-text-editor {
+    width: -webkit-fit-content;
+    width: -moz-fit-content;
+    width: fit-content;
+    height: -webkit-fit-content;
+    height: -moz-fit-content;
+    height: fit-content;
+    line-height: 1.5;
+  }
+
+  .c-text-editor .read-only-div.move {
+    cursor: move !important;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    caret-color: transparent;
+  }
+
+  .c-text-editor .read-only-div {
+    pointer-events: auto;
+    position: absolute;
+    padding: 5px;
+    border: 1px solid transparent;
+  }
 </style>
