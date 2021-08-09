7d366082f9f3eecde925c0acad007a8d61f75812
ReusLi <574399934@qq.com>
2021-08-09 21:25:49 +0800

    add code

--- src/components/svg/line-color/index.vue
+++ src/components/svg/line-color/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="c-draw-color-label" id="l1vcmYl6kDAl" style1="width: 15px; height: 33px; left: 224px; top: 224px;"
-    :style="{left: `${this.startInfo.x}px`, top: `${this.startInfo.y}px`}"
+    style="background-color: #e8e8e8; width: 10px; height: 10px;"
+    :style="{left: `${this.x1}px`, top: `${this.y1}px`}"
   >
     <svg width="15" :height="w1" class="svg-panel isShow">
       <polyline class="line-main" :points="p1" fill="none" stroke-width="1" stroke="rgba(249,86,113,1)" stroke-dasharray="none" style="cursor: move;">
@@ -56,6 +57,8 @@ export default defineComponent({
         y1 = this.startInfo.y,
         x2 = endInfoData.x,
         y2 = endInfoData.y;
+      // this.width = Math.abs(x1 - x2);
+      // this.height = Math.abs(y1 - y2);
       this.width = Math.abs(x1 - x2);
       this.height = Math.abs(y1 - y2);
 
@@ -64,8 +67,21 @@ export default defineComponent({
       this.x2 = Math.max(x1, x2);
       this.y2 = Math.max(y1, y2);
 
+      const midX = (this.width < this.height ? this.width : this.height) + 0.5,
+        endX = (this.width < this.height ? this.height : this.width) + 0.5;
+      // const w = x1 - x2;
+      // const h = y1 - y2;
+
+      const pointx1 = Math.abs(this.startInfo.x - this.x1) + 0.5,
+        pointy1 = pointx1;
+
       this.w1 = Math.min(this.width, this.height);
-      this.p1 = `0.5,0.5, ${this.w1 + 0.5} , ${this.w1 + 0.5}, ${this.w1 + 0.5}, ${this.height + 0.5}`
+      // this.p1 = `0.5,0.5, ${this.w1 + 0.5} , ${this.w1 + 0.5}, ${this.w1 + 0.5}, ${this.height + 0.5}`
+      if (this.width < this.height) {
+        this.p1 = `${pointx1}, ${pointx1}, ${midX},${midX}, ${midX},${endX}`
+      } else {
+        this.p1 = `${pointx1}, ${pointx1}, ${midX},${midX}, ${endX},${midX}`
+      }
     }
   }
 })
